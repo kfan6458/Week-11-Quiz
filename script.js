0d9b36ee8edf6ce937ea4b0c545a969dd80f5973
kfan6458 <133334668+kfan6458@users.noreply.github.com>
2023-05-15 01:13:31 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const svg = document.getElementById("svg-canvas");
+
+window.addEventListener("resize", resizeSvg);
+
+function resizeSvg(){
+    let bbox = svg.getBoundingClientRect();
+    svg.setAttribute("viewBox", `0 0 ${bbox.width} ${bbox.height}`);
+}
